95efa94174302111c257c7edce318a279c39d370
Luis Antunes <luis.antunes@thomsonreuters.com>
2018-03-29 05:23:43 -0400

    passing along cell index in demos

--- cellpylib_demo.py
+++ cellpylib_demo.py
@@ -5,6 +5,6 @@ cellular_automaton = ca.init_simple(200)
 
 # evolve the cellular automaton for n time steps
 cellular_automaton = ca.evolve(cellular_automaton, n_steps=100,
-                               apply_rule=lambda state: ca.nks_rule(state, 30))
+                               apply_rule=lambda state, c: ca.nks_rule(state, 30))
 
 ca.plot(cellular_automaton)
--- density_classification_demo.py
+++ density_classification_demo.py
@@ -9,6 +9,6 @@ print("density of 1s: %s" % (np.count_nonzero(cellular_automaton) / 149))
 rule_number = 6667021275756174439087127638698866559
 
 cellular_automaton = ca.evolve(cellular_automaton, n_steps=149,
-                               apply_rule=lambda state: ca.number_rule(state, rule_number), r=3)
+                               apply_rule=lambda state, c: ca.number_rule(state, rule_number), r=3)
 
 ca.plot(cellular_automaton)
