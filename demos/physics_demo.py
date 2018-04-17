d130bbd482ac8916f7ceeef4eae1c52aefba05ac
Luis Antunes <luis.antunes@thomsonreuters.com>
2018-04-16 20:54:17 -0400

    adding some comments to demo

--- demos/physics_demo.py
+++ demos/physics_demo.py
@@ -7,6 +7,27 @@ from pymunk import Vec2d
 import math, sys, random
 import numpy as np
 
+# TODO make sure total system energy is conserved (i.e. there should be no energy lost as "heat")
+# TODO should we ensure that no two balls start in the same place?
+"""
+From https://www.frontiersin.org/articles/10.3389/fmicb.2016.01437/full
+"Physics-Based Simulation Environment
+In order to match observations at the macroscopic level with physical qualities of individual cells, the simulation 
+strategy focused on discrete objects for both population(s) and medium to reproduce the motion of the bacteria through 
+soft agar. Instead of having a single object to simulate an expanding (growing and swimming) colony, the simulation 
+used several circular objects so that the overall structure could adapt its morphology to environmental constraints. 
+Importantly, any given cellular-object did not represent a single cell, but a group of cells.
+The simulation made use of the 2D rigid body physics library Chipmunk through its Python wrap Pymunk. This physics 
+engine takes care of all collisions among shapes happening in the model. High accuracy is needed in solving collisions 
+as the simulation relies on the effects of these events to shape the patterns. The simulation was built upon the 
+physics engine Chipmunk, that uses the Gilbert–Johnson–Keerthi algorithm (Gilbert et al., 1988) to calculate distances 
+between objects and the Expanding Polytope Algorithm to calculate penetrations (Bergen and Bergen, 2003). Upon 
+collisions, an impulse is generated on the objects (consequence of the mechanical stress) that pushes the objects apart 
+(measured in Figure 2C) and added to the velocity of the object to finally calculate the new moving vector."
+
+http://chipmunk-physics.net/
+http://www.pymunk.org
+"""
 
 pygame.init()
 screen = pygame.display.set_mode((600, 600))
