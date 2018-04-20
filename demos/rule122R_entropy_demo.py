124e60ece608f4c9c05ce2849d1403e5fca1fb55
Luis Antunes <luis.antunes@thomsonreuters.com>
2018-04-19 23:00:46 -0400

    adding average cell entropy plot to demo

--- demos/rule122R_entropy_demo.py
+++ demos/rule122R_entropy_demo.py
@@ -11,12 +11,14 @@ cellular_automaton = cpl.evolve(cellular_automaton, timesteps=1000, apply_rule=r
 timestep = []
 bientropies = []
 shannon_entropies = []
+average_cell_entropies = []
 apentropies = []
 for i, c in enumerate(cellular_automaton):
     timestep.append(i)
     bit_string = ''.join([str(x) for x in c])
     bientropies.append(cpl.ktbien(bit_string))
     shannon_entropies.append(cpl.shannon_entropy(bit_string))
+    average_cell_entropies.append(cpl.average_cell_entropy(cellular_automaton[:i+1]))
     apentropies.append(cpl.apen(bit_string, m=1, r=0))
     print("%s, %s, %s, %s" % (i, bientropies[-1], shannon_entropies[-1], apentropies[-1]))
 
@@ -33,6 +35,10 @@ plt.title("ApEn")
 plt.plot(timestep, apentropies)
 
 plt.figure(4)
+plt.title("Avg. Cell (Shannon) Entropy")
+plt.plot(timestep, average_cell_entropies)
+
+plt.figure(5)
 cpl.plot(cellular_automaton)
 
 
